c44cfb9f79a236e65ad1296d081f58d9c1408636
JohnGastone <johngastone11@gmail.com>
2024-06-05 22:10:04 +0300

    +++

---
+++ lib/Pages/liveCallPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LiveCallPage extends StatefulWidget {
+  const LiveCallPage({super.key});
+
+  @override
+  State<LiveCallPage> createState() => _LiveCallPageState();
+}
+
+class _LiveCallPageState extends State<LiveCallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
