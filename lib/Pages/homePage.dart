46493c38d0cf04be64fcb412fc7248089ca3d068
JohnGastone <johngastone11@gmail.com>
2024-06-05 17:25:35 +0300

    ++++

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,8 +22,25 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 80,
             ),
-            Text("Ukiwa na info NISANUE",
-                style: GoogleFonts.montserrat(fontSize: 30))
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Connections",
+                    style: GoogleFonts.spaceMono(
+                        fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Icon(CupertinoIcons.heart),
+                      Icon(CupertinoIcons.search)
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         )),
       ),
