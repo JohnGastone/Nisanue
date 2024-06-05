f26ad458feac96e4952ca737bbdb85ba664a63d8
JohnGastone <johngastone11@gmail.com>
2024-06-05 13:48:48 +0300

    +++

---
+++ lib/Pages/landingPage.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Landingpage extends StatefulWidget {
+  const Landingpage({super.key});
+
+  @override
+  State<Landingpage> createState() => _LandingpageState();
+}
+
+class _LandingpageState extends State<Landingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
