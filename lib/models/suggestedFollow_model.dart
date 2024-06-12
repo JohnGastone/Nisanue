f12cd1673d7f2b1679f46fbd2a9470b1ee30cf15
JohnGastone <johngastone11@gmail.com>
2024-06-12 20:30:46 +0300

    +++

--- lib/models/suggestedFollow_model.dart
+++ lib/models/suggestedFollow_model.dart
@@ -5,3 +5,40 @@ class SuggestedfollowModel {
 
   SuggestedfollowModel({this.profilePhoto, this.userName, this.followersCount});
 }
+
+class SuggestedFollowList {
+  static List<SuggestedfollowModel> getSuggestedFollow = [
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+    SuggestedfollowModel(
+      profilePhoto: 'assets/p1.png',
+      userName: 'Mama Kilakshal',
+      followersCount: '64 k',
+    ),
+  ];
+  static List<SuggestedfollowModel> displaySuggestedfollow =
+      List.from(SuggestedFollowList.getSuggestedFollow);
+}
