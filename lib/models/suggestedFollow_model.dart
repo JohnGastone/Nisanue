79eb5e9e2f47c79e2037e7fb87dd740a47a7f524
JohnGastone <johngastone11@gmail.com>
2024-06-12 20:22:46 +0300

    +++

---
+++ lib/models/suggestedFollow_model.dart
@@ -0,0 +1,7 @@
+class SuggestedfollowModel {
+  String? profilePhoto;
+  String? userName;
+  String? followersCount;
+
+  SuggestedfollowModel({this.profilePhoto, this.userName, this.followersCount});
+}
