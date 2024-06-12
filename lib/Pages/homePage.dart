59d9903b28328374c559139be81aca7d248ffb43
JohnGastone <johngastone11@gmail.com>
2024-06-12 22:30:35 +0300

    +++

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -120,9 +120,13 @@ class _ExplorePageState extends State<ExplorePage> {
                           const EdgeInsets.only(left: 20, top: 15, bottom: 10),
                       child: Row(
                         children: [
-                          CircleAvatar(
-                            child: Image.asset("./assets/man.png"),
-                          ),
+                          ClipOval(
+                              child: Image.asset(
+                            "./assets/p1.png",
+                            fit: BoxFit.cover,
+                            height: 60,
+                            width: 60,
+                          )),
                           SizedBox(
                             width: 8,
                           ),
@@ -215,9 +219,13 @@ class _ExplorePageState extends State<ExplorePage> {
                           const EdgeInsets.only(left: 20, top: 15, bottom: 10),
                       child: Row(
                         children: [
-                          CircleAvatar(
-                            child: Image.asset("./assets/man.png"),
-                          ),
+                          ClipOval(
+                              child: Image.asset(
+                            "./assets/p1.png",
+                            fit: BoxFit.cover,
+                            height: 60,
+                            width: 60,
+                          )),
                           SizedBox(
                             width: 8,
                           ),
@@ -310,9 +318,13 @@ class _ExplorePageState extends State<ExplorePage> {
                           const EdgeInsets.only(left: 20, top: 15, bottom: 10),
                       child: Row(
                         children: [
-                          CircleAvatar(
-                            child: Image.asset("./assets/man.png"),
-                          ),
+                          ClipOval(
+                              child: Image.asset(
+                            "./assets/p2.png",
+                            fit: BoxFit.cover,
+                            height: 60,
+                            width: 60,
+                          )),
                           SizedBox(
                             width: 8,
                           ),
